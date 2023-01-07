6eed4c207b059975a8cd026aec0529f6cee85b45
Rohaan Khan <rohaangrapher@gmail.com>
2023-01-07 22:04:43 +0500

    added the gateway.js which makes the API call for user

---
+++ gateway-aggregation/src/components/Gateway.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+function Gateway(props) {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(props.endpoint);
+      const data = await res.json();
+      setResponse(data);
+    }
+    fetchData();
+  }, [props.endpoint]);
+
+  return (
+    <div>
+      {response && (
+        <div>
+            {response.map( (item) =>
+                <div key={item.id} >
+                    <h1>{ item.id + ". " + item.title}</h1>
+                    <p>{item.description}</p>
+                </div>
+            )}
+          </div>
+      )}
+    </div>
+  );
+}
+
+export default Gateway;
